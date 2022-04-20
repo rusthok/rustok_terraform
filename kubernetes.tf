f955d82954040501aa80910574adcb1098bd1ddd
rusthok <rustok.sg@gmail.com>
2022-04-20 01:58:50 -0500

    prueba3.2

--- kubernetes.tf
+++ kubernetes.tf
@@ -3,4 +3,49 @@ provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   token                  = data.aws_eks_cluster_auth.cluster.token
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  ###podria borrarse
+  exec {
+    api_version = "client.authentication.k8s.io/v1alpha1"
+    args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
+    command     = "aws"
+  }
+  ######podria borrarse  
 }
+
+
+
+###podria borrarse
+variable "cluster_name" {
+  type = string
+}
+
+data "aws_eks_cluster" "example" {
+  name = var.cluster_name
+}
+
+data "aws_eks_cluster_auth" "example" {
+  name = var.cluster_name
+}
+
+# Create a local variable for the load balancer name.
+locals {
+  lb_name = split("-", split(".", kubernetes_service.example.status.0.load_balancer.0.ingress.0.hostname).0).0
+}
+
+# Read information about the load balancer using the AWS provider.
+data "aws_elb" "example" {
+  name = local.lb_name
+}
+
+output "load_balancer_name" {
+  value = local.lb_name
+}
+
+output "load_balancer_hostname" {
+  value = kubernetes_service.example.status.0.load_balancer.0.ingress.0.hostname
+}
+
+output "load_balancer_info" {
+  value = data.aws_elb.example
+}
+###podria borrarse
