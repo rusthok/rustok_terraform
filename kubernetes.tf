28b72300a2de6113f6db907863cdd993ec425bc6
rusthok <rustok.sg@gmail.com>
2022-04-20 17:22:33 -0500

    prueba4.14

--- kubernetes.tf
+++ kubernetes.tf
@@ -3,6 +3,13 @@ provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   token                  = data.aws_eks_cluster_auth.cluster.token
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  #uncomment below to activate LB
+  exec {
+    api_version = "client.authentication.k8s.io/v1alpha1"
+    args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
+    command     = "aws"
+  }
+  #uncomment above to activate lb
 }
 
 
